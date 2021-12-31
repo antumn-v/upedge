4d9d3fde9005030948596d5fef1a8d28eb6f0a95
秋与 <15321366154@163.com>
2021-12-31 14:56:43 +0800

    手动增加客户产品库存

---
+++ oms/src/main/java/com/upedge/oms/modules/stock/request/ManualAddCustomerStockRequest.java
@@ -0,0 +1,25 @@
+package com.upedge.oms.modules.stock.request;
+
+
+import com.upedge.oms.modules.stock.vo.CustomerSkuStockVo;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+public class ManualAddCustomerStockRequest {
+
+    @NotNull
+    Long productId;
+
+    @NotNull
+    Long customerId;
+
+    Integer warehouseId;
+
+    @Size(min = 1)
+    List<CustomerSkuStockVo> customerSkuStockVos;
+
+}
