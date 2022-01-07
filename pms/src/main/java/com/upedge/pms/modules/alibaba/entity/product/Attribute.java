2619140d171aed26326575b8b015dba90b1818ff
秋与 <15321366154@163.com>
2022-01-07 11:12:10 +0800

    导入1688产品

---
+++ pms/src/main/java/com/upedge/pms/modules/alibaba/entity/product/Attribute.java
@@ -0,0 +1,46 @@
+package com.upedge.pms.modules.alibaba.entity.product;
+
+
+public class Attribute {
+
+    private Long variantId;
+
+    private String attributeValue;
+
+    private String skuImageUrl;
+
+    private String attributeDisplayName;
+
+    public Long getVariantId() {
+        return variantId;
+    }
+
+    public void setVariantId(Long variantId) {
+        this.variantId = variantId;
+    }
+
+
+    public String getAttributeValue() {
+        return attributeValue;
+    }
+
+    public void setAttributeValue(String attributeValue) {
+        this.attributeValue = attributeValue;
+    }
+
+    public String getSkuImageUrl() {
+        return skuImageUrl;
+    }
+
+    public void setSkuImageUrl(String skuImageUrl) {
+        this.skuImageUrl = skuImageUrl;
+    }
+
+    public String getAttributeDisplayName() {
+        return attributeDisplayName;
+    }
+
+    public void setAttributeDisplayName(String attributeDisplayName) {
+        this.attributeDisplayName = attributeDisplayName;
+    }
+}
