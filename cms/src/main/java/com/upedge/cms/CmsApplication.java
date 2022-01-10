568c000d3c505759428dead08a32fd0a82f61594
秋与 <15321366154@163.com>
2022-01-10 15:24:33 +0800

    cms启动

--- cms/src/main/java/com/upedge/cms/CmsApplication.java
+++ cms/src/main/java/com/upedge/cms/CmsApplication.java
@@ -1,9 +1,22 @@
 package com.upedge.cms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@EnableTransactionManagement
+@EnableAsync
+@EnableFeignClients(basePackages = "com.upedge.common.feign")
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.upedge.cms.*","com.upedge.redis.*","com.upedge.common.*"})
+@MapperScan( "com.upedge.cms.modules.*.dao")
 public class CmsApplication {
 
     public static void main(String[] args) {
