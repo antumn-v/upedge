f552011067a42a831a0fd0d97e26212cd53e8e38
秋与 <15321366154@163.com>
2022-04-18 09:25:13 +0800

    sms返回数据格式转化

--- sms/src/main/java/com/upedge/sms/SmsApplication.java
+++ sms/src/main/java/com/upedge/sms/SmsApplication.java
@@ -1,13 +1,17 @@
 package com.upedge.sms;
 
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -20,6 +24,19 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages = {"com.upedge.sms.*","com.upedge.redis.*","com.upedge.common.*"})
 @MapperScan( "com.upedge.sms.modules.*.dao")
 public class SmsApplication {
+
+    @Bean("jackson2ObjectMapperBuilderCustomizer")
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        Jackson2ObjectMapperBuilderCustomizer customizer = new Jackson2ObjectMapperBuilderCustomizer() {
+            @Override
+            public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+                jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance)
+                        .serializerByType(Long.TYPE, ToStringSerializer.instance);
+            }
+        };
+        return customizer;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SmsApplication.class, args);
