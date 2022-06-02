0b644edad409fec9a05980642a362c5e7af211df
秋与 <15321366154@163.com>
2022-06-02 10:02:55 +0800

    1.客户名信息补充拦截器异常处理 2.账单金额异常修复

--- common-web/src/main/java/com/upedge/common/web/advice/ResponseObjectAspect.java
+++ common-web/src/main/java/com/upedge/common/web/advice/ResponseObjectAspect.java
@@ -3,8 +3,11 @@ package com.upedge.common.web.advice;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.upedge.common.constant.Constant;
 import com.upedge.common.constant.key.RedisKey;
+import com.upedge.common.model.user.vo.Session;
 import com.upedge.common.model.user.vo.UserVo;
+import com.upedge.common.web.util.UserUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
@@ -61,51 +64,59 @@ public class ResponseObjectAspect implements ResponseBodyAdvice {
 
     @Override
     public Object beforeBodyWrite(Object result, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
-        if (result == null){
-            return result;
-        }
-        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(result));
-        if (!jsonObject.containsKey("data")){
-            return result;
-        }
         try {
+            Session session = UserUtil.getSession(redisTemplate);
+            if (session == null
+            || session.getApplicationId() == Constant.APP_APPLICATION_ID){
+                return result;
+            }
+
+            if (result == null) {
+                return result;
+            }
+
+            JSONObject jsonObject = JSONObject.parseObject(result.toString());
+            if (!jsonObject.containsKey("data")) {
+                return result;
+            }
+
             Object data = jsonObject.get("data");
 
-            if (data instanceof JSONObject){
-                JSONObject jsonData = JSONObject.parseObject(JSON.toJSONString(data));
-                if (jsonData.containsKey("customerId")){
+            if (data instanceof JSONObject) {
+                JSONObject jsonData = JSONObject.parseObject(data.toString());
+                if (jsonData.containsKey("customerId")) {
                     Long customerId = jsonObject.getLong("customerId");
-                    if (customerId == null){
+                    if (customerId == null) {
                         return result;
                     }
-                    UserVo userVo = (UserVo) redisTemplate.opsForHash().get(RedisKey.STRING_CUSTOMER_INFO,String.valueOf(customerId));
-                    if (userVo != null){
-                        jsonData.put("username",userVo.getUsername());
+                    UserVo userVo = (UserVo) redisTemplate.opsForHash().get(RedisKey.STRING_CUSTOMER_INFO, String.valueOf(customerId));
+                    if (userVo != null) {
+                        jsonData.put("username", userVo.getUsername());
                     }
-                    jsonObject.put("data",jsonData);
+                    jsonObject.put("data", jsonData);
                 }
             }
 
-            if (data instanceof JSONArray){
-                JSONArray jsonArray = JSONArray.parseArray(JSON.toJSONString(data));
+            if (data instanceof JSONArray) {
+                JSONArray jsonArray = JSONArray.parseArray(data.toString());
                 List<Object> list = new ArrayList<>();
-                for (Object a : jsonArray){
+                for (Object a : jsonArray) {
                     JSONObject x = (JSONObject) JSON.toJSON(a);
                     Long customerId = x.getLong("customerId");
-                    if (customerId == null){
+                    if (customerId == null) {
                         return result;
                     }
-                    UserVo userVo = (UserVo) redisTemplate.opsForHash().get(RedisKey.STRING_CUSTOMER_INFO,String.valueOf(customerId));
-                    if (userVo != null){
-                        x.put("username",userVo.getUsername());
+                    UserVo userVo = (UserVo) redisTemplate.opsForHash().get(RedisKey.STRING_CUSTOMER_INFO, String.valueOf(customerId));
+                    if (userVo != null) {
+                        x.put("username", userVo.getUsername());
                     }
                     list.add(x);
                 }
-                jsonObject.put("data",list);
+                jsonObject.put("data", list);
             }
             return jsonObject;
         } catch (Exception e) {
-            e.printStackTrace();
+
         }
         return result;
     }
