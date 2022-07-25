a4d5ce11f49f9813729b93b9f7ef3b06eb3baec3
秋与 <15321366154@163.com>
2022-07-25 15:32:02 +0800

    手动出入库

--- pms/src/main/java/com/upedge/pms/modules/purchase/controller/VariantWarehouseStockController.java
+++ pms/src/main/java/com/upedge/pms/modules/purchase/controller/VariantWarehouseStockController.java
@@ -4,6 +4,7 @@ import com.upedge.common.base.BaseResponse;
 import com.upedge.common.component.annotation.Permission;
 import com.upedge.common.model.user.vo.Session;
 import com.upedge.common.web.util.UserUtil;
+import com.upedge.pms.modules.purchase.request.VariantStockExImRecordUpdateRequest;
 import com.upedge.pms.modules.purchase.request.VariantStockUpdateRequest;
 import com.upedge.pms.modules.purchase.request.VariantWarehouseStockListRequest;
 import com.upedge.pms.modules.purchase.service.VariantWarehouseStockService;
@@ -11,10 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -45,8 +43,21 @@ public class VariantWarehouseStockController {
     @Permission(permission = "purchase:variantwarehousestock:update")
     public BaseResponse add(@RequestBody @Valid VariantStockUpdateRequest request) {
         Session session = UserUtil.getSession(redisTemplate);
-
         return variantWarehouseStockService.updateVariantStock(request,session);
+    }
+
+    @ApiOperation("手动出库")
+    @PostMapping("/customEx")
+    public BaseResponse stockEx(@RequestBody@Valid VariantStockExImRecordUpdateRequest request){
+        Session session = UserUtil.getSession(redisTemplate);
+        return variantWarehouseStockService.variantStockEx(request,session);
+    }
+
+    @ApiOperation("手动入库")
+    @PostMapping("/customIm")
+    public BaseResponse stockIm(@RequestBody@Valid VariantStockExImRecordUpdateRequest request){
+        Session session = UserUtil.getSession(redisTemplate);
+        return variantWarehouseStockService.variantStockIm(request,session);
     }
 
 
